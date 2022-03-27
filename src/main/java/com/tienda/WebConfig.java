fe78bc29273649a5f79c1f8f2cc879d32d0c29d0
Angelo Sotomayor <assv.9330@gmail.com>
2022-03-26 19:01:11 -0600

    Serie de Videos de semana 09 internacionalización

---
+++ src/main/java/com/tienda/WebConfig.java
@@ -0,0 +1,47 @@
+/*
+Internacionalizacion -i18n - 18 letras entre I y n
+Locale: Representa el lenguaje, la region geográfica, variantes del dialecto/idioma,
+de un usuario
+SessionLocaleResolver: guardar el Locale seleccionado por un usuario como atributo
+en el request HTTP
+LocaleChangeInterceptor: Detectar  cualquier cambio de parte del usuario hacia lo
+que es el Locale
+ */
+package com.tienda;
+
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration/*Indica que la clase es de tipo configuración*/
+/*Ésta clase maneja toda la internacionalización de la página*/
+public class WebConfig implements WebMvcConfigurer{/*Clase que forma parte del
+    framework de Spring*/
+    @Bean/*Ésta anotación indica que lo que se encuentre dentro de la clase, se 
+    creará automáticamente dentro del ambiente de springboot*/
+    public SessionLocaleResolver localeResolver(){/*Se está seteando por defecto
+        que el idioma de la página es inglés, es el solucionador de ubicaciones*/
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("en"));/*Establece que el idioma 
+        predeterminado es español*/
+        return slr;
+    }
+    
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){/*La clase actua
+        como una intersección dinámica entre los cambios de idioma*/
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");/*"Lang"=Lenguaje se utiliza como parámetro para
+        configurar el lenguaje*/
+        return lci;
+    }
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registro){
+        registro.addInterceptor(localeChangeInterceptor());
+    }
+}
--- src/main/java/com/tienda/controller/ArticuloController.java
+++ src/main/java/com/tienda/controller/ArticuloController.java
@@ -2,6 +2,7 @@
 
 import com.tienda.domain.Articulo;
 import com.tienda.service.ArticuloService;
+import com.tienda.service.CategoriaService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,6 +18,8 @@ public class ArticuloController {
     @Autowired/*Permite enlazar un objeto*/
     private ArticuloService articuloService;
     //Se invoca la interfaz y spring busca la clase que la implementa en automático
+    @Autowired
+    private CategoriaService categoriaService;
     @GetMapping("/articulo/listado")/*"Mapeo del URL"La bronca es aquí en index controller*/
     public String incio(Model model){/*Luego de mapear la URL, se genera el 
         método el cual puede tener cualquier nombre. Es decir, el método se 
@@ -36,7 +39,9 @@ public class ArticuloController {
     }
     
     @GetMapping("/articulo/nuevo")
-    public String nuevoArticulo(Articulo articulos){
+    public String nuevoArticulo(Articulo articulos, Model model){
+        var categorias = categoriaService.getCategoria(true);
+        model.addAttribute("categorias", categorias);
         return "articulo/modificar";
     }
     
@@ -48,6 +53,7 @@ public class ArticuloController {
     
     @GetMapping("/articulo/modificar/{idarticulo}")
     public String modificarArticulo(Articulo articulos, Model model){
+        var categorias = categoriaService.getCategoria(true);
         articulos = articuloService.getArticulo(articulos);
         model.addAttribute("articulo", articulos);
         return "articulo/modificar";
